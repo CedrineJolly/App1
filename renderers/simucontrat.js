593fd240138a2ee0ff2a6922f4418091047b8bc4
{Cedrine Jolly} <{cjolly002@ensc.fr}>
2024-02-27 18:32:34 +0100

    CAF et PropPerso OK

--- renderers/simucontrat.js
+++ renderers/simucontrat.js
@@ -13,8 +13,9 @@ export const displaySimuContrats = () => {
     let Salaire = 0;
     const TarifHoraire = parseFloat(contrat.TarifHoraire);
     const NbSemaines = parseFloat(contrat.NbSemaines);
-    let MontantAide = 0;
+    let MontantAide = 319.07;
     let SalaireCAF = 0;
+    let CongesPayes = 0;
 
     html += `<h6 class="contrat-text"> Temps total par semaines : ${TpsTot} heures </h6>`
     //Affichage du salaire selon le type de contrat
@@ -25,16 +26,19 @@ export const displaySimuContrats = () => {
     else if (contrat.Type == 'AI'){
       Salaire = ((TpsTot * TarifHoraire * NbSemaines)/12).toFixed(2);
       html += `<h6 class="contrat-text"> Salaire fixe net à payer par mois : ${Salaire} € </h6>`
+      CongesPayes = (Salaire * 12 /50).toFixed(2); 
+      html += `<h6 class="contrat-text"> Montant des congés payés annuels ramenés à la semaine : ${CongesPayes} € </h6>`
     }
     else{
-      Salaire = 0 ; //à compléter
+      //Salaire ramené à 47 semaines (52 - 5 semaines de congés payés) 
+      Salaire = ((((TpsTot * NbSemaines)/47)*TarifHoraire*52)/12).toFixed(2);
       html += `<h6 class="contrat-text"> Salaire fixe net à payer par mois : ${Salaire} € </h6>`
     }
 
     //Calcul des aides et déduction du salaire net
     html += `<p></p> <h6 class="contrat-text"; style="color: #2c3e50";> Aide CAF </h6>`
     html += `<p></p> <h6 class="contrat-text"> Vous avez le droit à ${MontantAide} € d'aide de la CAF </h6>`
-    SalaireCAF = Salaire - MontantAide;
+    SalaireCAF = (Salaire - MontantAide).toFixed(2);
     html += `<h6 class="contrat-text"> Salaire restant à payer après déduction : ${SalaireCAF} € </h6>`
 
     return html

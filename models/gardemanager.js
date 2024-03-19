56e89b49d4892fb165242b78e7108c80c946d270
{Cedrine Jolly} <{cjolly002@ensc.fr}>
2024-03-19 09:03:49 +0100

    modifs de ce matin

--- models/gardemanager.js
+++ models/gardemanager.js
@@ -5,7 +5,7 @@ export const putGarde = (gardeData) =>
 {
     console.log(gardeData);
     const stmt = db.prepare('INSERT INTO Garde (Date, NbHeures, IdEnfant, HArriv, HDepart) VALUES (?, ?, ?, ?, ?)');
-    const info = stmt.run(gardeData.date, gardeData.nbHeures, gardeData.idEnfant);
+    const info = stmt.run(gardeData.date, gardeData.nbHeures, gardeData.idEnfant, gardeData.hArriv, gardeData.hDepart);
     if(info.changes == 1) {
         return info.lastInsertRowid
     }
